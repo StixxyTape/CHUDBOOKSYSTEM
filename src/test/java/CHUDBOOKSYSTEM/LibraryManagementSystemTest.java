220b512505d39f63df2fbf9755f69090cebf858b
Stixxy <100129571+StixxyTape@users.noreply.github.com>
2026-04-20 14:29:09 +0100

    Can now use test cases

---
+++ src/test/java/CHUDBOOKSYSTEM/LibraryManagementSystemTest.java
@@ -0,0 +1,9 @@
+package CHUDBOOKSYSTEM;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+
+class LibraryManagementSystemTest {
+
+}
